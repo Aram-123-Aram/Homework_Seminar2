96f7a71767b249646ef840aaef57dad8ff2f572a
Aram <armsir-8182@mail.ru>
2022-12-24 20:22:22 +0300

    Changing random positions in list

---
+++ Exercise5_sem2_homework.py
@@ -0,0 +1,15 @@
+'''
+Реализуйте алгоритм перемешивания списка. (рандомно поменять местами элементы списка между собой)
+'''
+list = ['Hi', 456, 4.5, [2, 4, 1,] ]
+print(list)
+import random
+i1 = random.randint(0,3)
+i2 = random.randint(0,3)
+print(f"{i1} позицию меняем с позицией {i2}: Получаем")
+for i in range(len(list)):
+    if i == i1:
+        temp=list[i1]
+        list[i1]=list[i2]
+        list[i2]=temp
+print(list)
